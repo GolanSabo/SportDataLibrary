393cb58c0fcf11168405a202af7969266fae23c8
Golan Sabo <golansabo@yahoo.com>
2015-08-26 21:15:24 +0300

    Version 1.3A

---
+++ SoccerDataLibrary/Test/Models/LeagueTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using SoccerDataLibrary;
+using SoccerDataLibrary.Utils;
+using SoccerDataLibrary.Enums;
+using SoccerDataLibrary.Exceptions;
+
+namespace SportDataLibrary.Test
+{
+    [TestFixture]
+    class LeagueTests
+    {
+        [TestCase]
+        public void GetLeagueListTest()
+        {
+            Assert.IsNotNull(SecondaryLeaguesDictionaryCreator.GetLeagueList());
+        }
+
+        [TestCase]
+        public void GetLeagueTeamsTest()
+        {
+            League league = new League(LeagueNameId.ENGLAND);
+            Assert.IsNotNull(league.GetLeagueTeams());
+        }
+
+        [TestCase]
+        [ExpectedException(typeof(TeamNotFoundException))]
+        public void GetLeagueTeamsWithExceptionTest()
+        {
+            League league = new League(398);
+            Assert.IsNotNull(league.GetTeamByName("Barca"));
+        }
+
+        [TestCase]
+        public void GetTeamByNameTest()
+        {
+            League league = new League(LeagueNameId.ENGLAND);
+            Assert.IsNotNull(league.GetTeamByName("Manchester United FC"));
+        }
+    }
+}
---
+++ SoccerDataLibrary/Test/Models/TeamTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using SoccerDataLibrary;
+using SoccerDataLibrary.Utils;
+using SoccerDataLibrary.Enums;
+using SoccerDataLibrary.Exceptions;
+
+namespace SportDataLibrary.Test
+{
+    [TestFixture]
+    class TeamTests
+    {
+        [TestCase]
+        public void GetPlayerByJerseyNumberTest()
+        {
+            Assert.IsNotNull(new League(LeagueNameId.ENGLAND).GetTeamByName("Manchester United FC")["Manchester United FC"].GetPlayerByJerseyNumber(7));
+        }
+
+
+        [TestCase]
+        public void GetPlayerByNameTest()
+        {
+            Assert.IsNotNull(new League(LeagueNameId.ENGLAND).GetTeamByName("Manchester United FC")["Manchester United FC"].GetPlayerByName("Wayne Rooney"));
+        }
+
+
+        [TestCase]
+        [ExpectedException(typeof(PlayerNotFoundException))]
+        public void GetPlayerByJerseyNumberWithExceptionTest()
+        {
+            Assert.IsNotNull(new League(LeagueNameId.ENGLAND).GetTeamByName("Manchester United FC")["Manchester United FC"].GetPlayerByJerseyNumber(7000));
+        }
+
+        [TestCase]
+        [ExpectedException(typeof(PlayerNotFoundException))]
+        public void GetPlayerByNameWithExceptionTest()
+        {
+            Assert.IsNotNull(new League(LeagueNameId.ENGLAND).GetTeamByName("Manchester United FC")["Manchester United FC"].GetPlayerByName("Yaniv Sapir"));
+        }
+    }
+}
